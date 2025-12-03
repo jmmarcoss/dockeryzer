6a29eb2ccbb227d88371c32c6360fd6ce3723a0a
João Marcos <joaoma@ciandt.com>
2025-12-03 00:19:28 -0300

    refactor: add detected in langchain command

--- src/utils/langchain.go
+++ src/utils/langchain.go
@@ -2,19 +2,37 @@ package utils
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/jorgevvs2/dockeryzer/src/config"
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
 )
 
 func CreateDockerfileWithLangChain(ignoreComments bool) {
 
+	apiKey := config.APIKey
+	if apiKey == "" {
+		log.Fatal("API key not set in binary. Please rebuild with -ldflags")
+	}
+
 	projectTree, err := GetProjectStructure()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	tech := DetectProjectSmart(apiKey)
+
+	fmt.Printf("🔍 Detected: %s", tech.Language)
+	if tech.Framework != "" {
+		fmt.Printf(" (%s)", tech.Framework)
+	}
+	if tech.PackageManager != "" {
+		fmt.Printf(" [%s]", tech.PackageManager)
+	}
+	fmt.Println()
 
 	prompt := BuildDockerfilePrompt(projectTree, ignoreComments)
 
